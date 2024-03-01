9b4c3169ceb1282aa9da8fa79f8063dc6097364a
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-01 15:57:31 +0530

    Done the Scrolling part...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import './App.css';
 
 function App() {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="App">
       <div className='navbar'>
         <ul>
-          <li className='link'>Home</li>
-          <li className='link'>Services</li>
-          <li className='link'>Blog</li>
-          <li className='link'>Contact</li>
+          <li onClick={() => scrollToSection('home')} className='link'>Home</li>
+          <li onClick={() => scrollToSection('services')} className='link'>Services</li>
+          <li onClick={() => scrollToSection('blog')} className='link'>Blog</li>
+          <li onClick={() => scrollToSection('contact')} className='link'>Contact</li>
         </ul>
       </div>
-      <div className='home'>
+      <div className='home' id='home'>
         Home
       </div>
-      <div className='services'>
+      <div className='services' id='services'>
         Services
       </div>
-      <div className='blog'>
+      <div className='blog' id='blog'>
         Blog
       </div>
-      <div className='contact'>
+      <div className='contact' id='contact'>
         Contact
       </div>
     </div>
